6de6f2fce19c0f206e19d45254e67c8b564936dd
Stopher Malik <malikstopher@gmail.com>
2026-07-15 15:27:38 +0200

    fix: type error in Header menu animation ease property

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ const menuItemVariants = {
   open: (i: number) => ({
     opacity: 1,
     y: 0,
-    transition: { delay: 0.06 * i, duration: 0.3, ease: [0.25, 0.46, 0.45, 0.94] },
+    transition: { delay: 0.06 * i, duration: 0.3, ease: [0.25, 0.46, 0.45, 0.94] as const },
   }),
 };
 
